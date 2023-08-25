b00f1fdd38eb4a7aa92bc6dae94d5ee3cc89f6e5
bhaweshgit06 <bhaweshagrawal0@gmail.com>
2023-08-25 15:42:50 +0530

    updated

--- script.js
+++ script.js
@@ -29,51 +29,6 @@ function smoothScroll(targetSection){
    window.scrollBy(0, 50)
 }
 
-//skills autofill
-var progressBar = document.querySelectorAll('.skill-progress > div');
-var skillsContainer = document.getElementById('skills-container');
-window.addEventListener('scroll', checkScroll);
-
-var animationDone = false;
-
-function initializeBars(){
-   for(let bar of progressBar)
-{
-   bar.style.width = 0 + '%';
-}
-}
-
-function fillBars(){
-   for(let bar of progressBar)
-{
-  let targetWidth = bar.getAttribute('data-bar-width');
-
-  let currentWidth = 0;
-
-  let interval = setInterval(function(){
-if (currentWidth > targetWidth){
-   clearInterval(interval)
-   return;
-
-}
-currentWidth ++;
-bar.style.width = currentWidth + '%';
-  },5);
-
-}
-}
-
-
-function checkScroll(){
-   var coordinates = skillsContainer.getBoundingClientRect();
-if(!animationDone && coordinates.top <= window.innerHeight ){
-   animationDone = true;
- fillBars();
-}else if(coordinates.top > window.innerHeight){
-   animationDone = false;
-   initializeBars();
-}
-}
 
 
 
